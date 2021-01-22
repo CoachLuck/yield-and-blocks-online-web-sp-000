e915be97114c1ba8bb01546dda7b391adb6b4b92
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-22 19:31:02 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,4 +3,8 @@ def hello_t
 end
 
 # call your method here!
-
+def yielding_with_arguments(num)
+  puts "the program is executing the code inside the method"
+  yield(num)
+  puts "now we are back in the method"
+end
